c65c65686c77770d6ba59e3b4d1a93cc42ac0fc4
SELAULE <nselaule@student.wethinkcode.com>
2019-10-02 00:07:39 +0200

    response from server good

--- client.c
+++ client.c
@@ -11,25 +11,29 @@ void    errornclose(char *msg, int sock) {
     exit(1);
 }
 
-int     main (void) {
+int     main (int argc, char **argv) {
     int     sock;
     struct  sockaddr_in server;
     char    buff[1024];
 
+
+    if (argc != 2) {
+        error("No port given");
+    }
     sock = socket (AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         error("Socket failed");
     }
-    
-    server.sin_family = AF_INET;
 
-    server.sin_port = htons (PORT);
+    server.sin_family = AF_INET;
+    server.sin_port = htons (atoi(argv[1]));
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (connect (sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
         errornclose ("connect failed", sock);
     }
 
-    printf("Send a message");
+    printf("Send a message\n");
     while(1) {
         printf("Client: \t");
         scanf("%s", buff);
--- server.c
+++ server.c
@@ -1,11 +1,7 @@
 #include "socket.h"
 
-// void    error(char *msg) {
-//     perror(msg);
-//     exit(1);
-// }
 
-int     main(void) {
+int     main(int argc, char **argv) {
 
     int     sock;
     struct  sockaddr_in server;
@@ -15,6 +11,10 @@ int     main(void) {
     socklen_t   addr_size;
     pid_t   childId;
 
+    if (argc != 2) {
+        error("No port given");
+        // exit(1);
+    }
     sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         error("Failed to connect");
@@ -22,15 +22,15 @@ int     main(void) {
 
     memset(&server, '\0', sizeof(server));
     server.sin_family = AF_INET;
-    server.sin_port = htons(5000);
+    server.sin_port = htons(atoi(argv[1]));
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     if (bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
         error("Failed to bind");
     }
-    listen (sock, 5);
+    listen (sock, 10);
      while (1) {
-        mysock = accept (sock, (struct sockaddr *) &newAddr, &addr_size);
+        mysock = accept(sock, (struct sockaddr *) &newAddr, &addr_size);
         if (mysock < 0)
             error("Accept failed");
         printf("Connected to %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
@@ -39,12 +39,14 @@ int     main(void) {
             close(sock);
             while(1) {
                 recv(mysock, buff, sizeof(buff), 0);
-                if (strcmp(buff, "exit")) {
+                if (strcmp(buff, "exit") == 0) {
                     printf("Disconnecting from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
                     break ;
                 } else {
-                    printf ("Client: %s\n", buff);
-                    send(sock, buff, strlen(buff), 0);
+                    printf("Client: %s\n", buff);
+                    if (send(mysock, buff, strlen(buff), 0) == -1) {
+                        printf("Failed to send");
+                    }
                     bzero(buff, sizeof(buff));
                 }
             }
